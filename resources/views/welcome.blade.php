08a659dd6bce857a06af2060435d98aa38d513a8
leo.gerard <leo.gerard@GERARDL-N>
2024-09-18 16:44:13 +0200

    CRUD Motif, Absence
    Welcome perfect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="bg-blue-50 text-blue-500 dark:bg-blue-900 dark:text-blue-500">
+        <div class="bg-grey-50 text-grey-500 dark:bg-grey-900 dark:text-grey-500">
             {{-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> --}}
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-blue-600 selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
@@ -26,13 +26,13 @@
                             <!-- Your existing logo here -->
                         </div>
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <a class="border-2 mr-2 border-blue-600 text-blue-600 px-4 py-2 inline-block rounded-lg hover:bg-blue-600 hover:text-white" href="{{ route('absence.index') }}">
+                            <a class="border-2 mr-2 border-green-600 text-green-600 px-4 py-2 inline-block rounded-lg hover:bg-green-600 hover:text-white" href="{{ route('absence.index') }}">
                                 Absences
                             </a>
                             <a class="border-2 mr-2 border-blue-600 text-blue-600 px-4 py-2 inline-block rounded-lg hover:bg-blue-600 hover:text-white" href="{{route('user.index')}}">
                                 Utilisateurs
                             </a>
-                            <a class="border-2 border-blue-600 text-blue-600 px-4 py-2 inline-block rounded-lg hover:bg-blue-600 hover:text-white" href="{{route('motif.index')}}">
+                            <a class="border-2 border-red-600 text-red-600 px-4 py-2 inline-block rounded-lg hover:bg-red-600 hover:text-white" href="{{route('motif.index')}}">
                                 Motif
                             </a>
                         </div>
@@ -41,7 +41,7 @@
 
                     <!-- Your existing main content here -->
 
-                    <footer class="py-16 text-center text-sm text-blue-500 dark:text-blue-500">
+                    <footer class="py-16 text-center text-sm text-white-500 dark:text-white-500">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
                 </div>
