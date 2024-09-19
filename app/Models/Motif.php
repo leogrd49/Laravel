7750131306299f807d9825ab37955a10f99c2792
leo.gerard <leo.gerard@GERARDL-N>
2024-09-19 10:16:54 +0200

    SoftDelete only on Motif

--- app/Models/Motif.php
+++ app/Models/Motif.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  *
@@ -26,6 +27,7 @@ use Illuminate\Database\Eloquent\Model;
 class Motif extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     // protected $fillable = ['libelle'];
 
