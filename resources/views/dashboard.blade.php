1a3f3c198ab0109535bb7bbce1e9aab244ef83b1
leo.gerard <leo.gerard@GERARDL-N>
2024-09-23 11:25:42 +0200

    save

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,6 @@
         <p class=" text-gray-900"><a href="{{ url('/welcome') }}">Accueil</a></p>
     </x-slot>
 
-
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
